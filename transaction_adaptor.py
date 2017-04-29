c977addcf78301ecdfd18ab6e740be42c93a8fdd
Melissa Ko <mesako@stanford.edu>
2017-04-29 15:11:00 -0700

    get transaction info from CSV export from banks: Chase, Barclay, Discover

--- transaction_adaptor.py
+++ transaction_adaptor.py
@@ -1,3 +1,142 @@
 
-Discover
-Trans. Date
+# Discover
+# export as CSV file
+# Trans. Date
+# Post Date
+# Description
+# Amount
+# Category
+# Automotive
+    # auto service
+    # auto maintenance
+    # auto repairs
+    # auto supplies
+# Department Stores
+# Education
+    # schools
+    # day-care
+    # trade schools
+    # universities
+# Gasoline
+# Government Services
+    # taxes
+    # city services and fees
+    # traffic tickets
+# Home Improvement
+    # hardware stores
+    # paint stores
+    # garden centers
+    # flooring warehouses
+# Medical Services
+    # doctor visits
+    # insurance copays
+    # prescriptions
+# Merchandise
+    # apparel
+    # beauty products
+    # music
+# Others and Miscellaneous
+# Restaurants
+    # cafes
+    # fast food
+    # coffee shops
+# Services
+    # utilities
+    # shipping
+    # salons
+    # dry cleaning
+# Supermarkets
+# Travel and Entertainment
+    # air travel
+    # hotels
+    # accommodations
+    # tickets for events
+    # concerts
+    # sporting events
+    # movies
+# Warehouse Clubs
+    # Sam's Club
+    # Costco
+    # BJs
+
+
+# Chase
+# export as ??? PDF file
+# Date
+# Merchant
+# Amount
+# Category
+# Grocery Stores & Drugstores
+# Automotive
+    # Gas Stations
+# Shopping
+    # Department & Clothing Stores/Catalogs
+    # Wholesale Clubs & Discount Stores
+    # Office Supply Stores
+    # Electronic & Appliance Stores
+    # Furniture & Decore Stores
+    # Other Retail
+# Travel & Entertainment
+    # Transit
+# Home Improvement & Maintenance
+    # Landscaping
+    # Utilities
+# Tax Deductible & Related Expenses
+    # Charities & Other Organizations
+    # Education & Child Care Services
+    # Medical Supply Stores
+    # Services
+# Restaurants
+# Other Expenditures
+    # Dry Cleaners and Laundromats
+    # Health Clubs & Memberships
+    # Pet Supply Stores & Services
+    # Salons & Beauty Supply Stores
+    # Insurance
+    # Tax
+    # Legal & Financial Services
+    # Shipping and Storage
+    # Miscellaneous Services
+    # Other
+
+
+# BarclayCard
+# export as CSV file
+# header with Barclays Bank Delaware
+# Account number (last four digits)
+# Account Balance as of DATE: AMOUNT
+# Transaction Date
+# Description
+# Amount
+# Category
+# DEBIT OR CREDIT
+
+# Category
+# Automotive
+    # oil changes
+    # car washes
+    # tires
+# Healthcare
+# doctor visits
+# prescriptions
+# Merchandise
+    # mall
+    # online
+    # other retail venues
+# Restaurants & Entertainment
+    # meals
+    # movies
+    # sporting events
+# Travel
+# airlines
+    # car rentals
+    # lodging
+    # railroad tickets
+    # cruises
+# Everyday Spending
+    # gas
+    # groceries
+    # utilities
+# Other
+    # home improvement
+    # education
