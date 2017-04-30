2255c71def0c943a64c305fef19193475faab02e
Melissa Ko <mesako@stanford.edu>
2017-04-29 17:44:13 -0700

    adaptors for barclay, chase, and discover

---
+++ categorize_transactions.py
@@ -0,0 +1,157 @@
+
+# Category object
+# properties
+# tags = keywords
+# desc = string description of category
+
+categories = {}
+categories["Food"] = {"Food", "Restaurants", "Grocery Store", "Groceries", "Supermarket", "Market",
+    "Coffee Shop", "Fast Food", "Snacks", "Dessert", "Drinks", "Bars", "Alcohol", "Cafe", "Dining"}
+categories["Travel"] = {"Travel", "Cruise", "Hotel", "Lodging", "Accommodations",
+    "Air Travel", "Flights", "Airlines", "Rental Car", "Taxi", "Vacation"}
+categories["Car and Transit"] = {"AAA", "Gas", "Fuel", "Car Insurance", "Auto",
+    "Parking", "Mechanic", "Auto Repair", "Train", "Subway", "Metro"}
+categories["Bills and Utilities"] = {"Housing", "Rent", "Energy", "Electricity",
+    "Heat", "Water", "Garbage", "Internet", "Phone", "Renter's Insurance", "Television"}
+categories["Health"] = {"Dentist", "Doctor", "Prescription", "Medication",
+    "Pharmacy", "Eyecare", "Health Insurance", "Copay"}
+categories["Fun"] = {"Fun", "Entertainment", "Arts", "Music", "Movies", "Hobbies",
+    "Spa", "Games"}
+categories["Savings"] = {"House", "IRA", "Brokerage", "Investments", "Wedding",
+    "Honeymoon"}
+categories["Life and Household Expenses"] = {"Clothes", "Household Supplies",
+    "Personal Care", "Charity", "Gifts", "Shopping", "Education", "Books",
+    "Electronics", "Laundry", "Hair", "Salon", "Donations"}
+categories["Taxes"] = {"Federal", "State", "Tax"}
+categories["Income"] = {"Income", "Paycheck", "Gift", "Bonus", "Investment",
+    "Interest"}
+
+# Reimbursement	
+# Refund
+# Split
+
+
+
+### Discover ### 
+# Category
+# Automotive
+    # auto service
+    # auto maintenance
+    # auto repairs
+    # auto supplies
+# Department Stores
+# Education
+    # schools
+    # day-care
+    # trade schools
+    # universities
+# Gasoline
+# Government Services
+    # taxes
+    # city services and fees
+    # traffic tickets
+# Home Improvement
+    # hardware stores
+    # paint stores
+    # garden centers
+    # flooring warehouses
+# Medical Services
+    # doctor visits
+    # insurance copays
+    # prescriptions
+# Merchandise
+    # apparel
+    # beauty products
+    # music
+# Others and Miscellaneous
+# Restaurants
+    # cafes
+    # fast food
+    # coffee shops
+# Services
+    # utilities
+    # shipping
+    # salons
+    # dry cleaning
+# Supermarkets
+# Travel and Entertainment
+    # air travel
+    # hotels
+    # accommodations
+    # tickets for events
+    # concerts
+    # sporting events
+    # movies
+# Warehouse Clubs
+    # Sam's Club
+    # Costco
+    # BJs
+
+
+### Chase ###
+# Category
+# Grocery Stores & Drugstores
+# Automotive
+    # Gas Stations
+# Shopping
+    # Department & Clothing Stores/Catalogs
+    # Wholesale Clubs & Discount Stores
+    # Office Supply Stores
+    # Electronic & Appliance Stores
+    # Furniture & Decore Stores
+    # Other Retail
+# Travel & Entertainment
+    # Transit
+# Home Improvement & Maintenance
+    # Landscaping
+    # Utilities
+# Tax Deductible & Related Expenses
+    # Charities & Other Organizations
+    # Education & Child Care Services
+    # Medical Supply Stores
+    # Services
+# Restaurants
+# Other Expenditures
+    # Dry Cleaners and Laundromats
+    # Health Clubs & Memberships
+    # Pet Supply Stores & Services
+    # Salons & Beauty Supply Stores
+    # Insurance
+    # Tax
+    # Legal & Financial Services
+    # Shipping and Storage
+    # Miscellaneous Services
+    # Other
+
+
+### BarclayCard ###
+# Category (not included in CSV)
+# Automotive
+    # oil changes
+    # car washes
+    # tires
+# Healthcare
+# doctor visits
+# prescriptions
+# Merchandise
+    # mall
+    # online
+    # other retail venues
+# Restaurants & Entertainment
+    # meals
+    # movies
+    # sporting events
+# Travel
+# airlines
+    # car rentals
+    # lodging
+    # railroad tickets
+    # cruises
+# Everyday Spending
+    # gas
+    # groceries
+    # utilities
+# Other
+    # home improvement
+    # education
+
--- transaction_adaptor.py
+++ transaction_adaptor.py
@@ -1,142 +1,94 @@
 
-# Discover
-# export as CSV file
-# Trans. Date
-# Post Date
-# Description
-# Amount
-# Category
-# Automotive
-    # auto service
-    # auto maintenance
-    # auto repairs
-    # auto supplies
-# Department Stores
-# Education
-    # schools
-    # day-care
-    # trade schools
-    # universities
-# Gasoline
-# Government Services
-    # taxes
-    # city services and fees
-    # traffic tickets
-# Home Improvement
-    # hardware stores
-    # paint stores
-    # garden centers
-    # flooring warehouses
-# Medical Services
-    # doctor visits
-    # insurance copays
-    # prescriptions
-# Merchandise
-    # apparel
-    # beauty products
-    # music
-# Others and Miscellaneous
-# Restaurants
-    # cafes
-    # fast food
-    # coffee shops
-# Services
-    # utilities
-    # shipping
-    # salons
-    # dry cleaning
-# Supermarkets
-# Travel and Entertainment
-    # air travel
-    # hotels
-    # accommodations
-    # tickets for events
-    # concerts
-    # sporting events
-    # movies
-# Warehouse Clubs
-    # Sam's Club
-    # Costco
-    # BJs
+import csv
+import string
+import datetime
 
 
-# Chase
-# export as ??? PDF file
-# Date
-# Merchant
-# Amount
-# Category
-# Grocery Stores & Drugstores
-# Automotive
-    # Gas Stations
-# Shopping
-    # Department & Clothing Stores/Catalogs
-    # Wholesale Clubs & Discount Stores
-    # Office Supply Stores
-    # Electronic & Appliance Stores
-    # Furniture & Decore Stores
-    # Other Retail
-# Travel & Entertainment
-    # Transit
-# Home Improvement & Maintenance
-    # Landscaping
-    # Utilities
-# Tax Deductible & Related Expenses
-    # Charities & Other Organizations
-    # Education & Child Care Services
-    # Medical Supply Stores
-    # Services
-# Restaurants
-# Other Expenditures
-    # Dry Cleaners and Laundromats
-    # Health Clubs & Memberships
-    # Pet Supply Stores & Services
-    # Salons & Beauty Supply Stores
-    # Insurance
-    # Tax
-    # Legal & Financial Services
-    # Shipping and Storage
-    # Miscellaneous Services
-    # Other
+def discover_adaptor(file_path : str):
+    '''
+    Discover
+    export as CSV file
+    Trans. Date, Post Date, Description, Amount, Category
+    '''
+    transaction_list = []
+    with open(file_path) as discover_csv:
+        discover_data = list(csv.DictReader(discover_csv))
+    
+    for row in discover_data:
+        transaction_list.append(make_transaction_from_discover(row))
+    return transaction_list
+
+def make_transaction_from_discover(discover_transaction):
+    date = change_date(discover_transaction["Trans. Date"])
+    amount = change_amount(discover_transaction["Amount"])
+    merchant = change_merchant(discover_transaction["Description"])
+    transaction_object = Transaction(date = date, amount = amount, merchant = merchant)
+    return transaction_object
+
+def change_date(trans_date : str):
+    correct_date = trans_date.split(sep = "/")
+    correct_date = list(map(int, correct_date))
+    correct_date = month_day_year_to_date(*correct_date)
+    return correct_date
+
+def month_day_year_to_date(month, day, year):
+    return date(year, month, day)
+
+def change_amount(amount : str):
+    correct_amount = float(amount)
+    return correct_amount
+
+def change_merchant(merchant : str):
+    regex = re.compile('[^a-zA-Z -]')
+    correct_merchant = regex.sub('', merchant).title()
+    return correct_merchant
 
 
-# BarclayCard
-# export as CSV file
-# header with Barclays Bank Delaware
-# Account number (last four digits)
-# Account Balance as of DATE: AMOUNT
-# Transaction Date
-# Description
-# Amount
-# Category
-# DEBIT OR CREDIT
+def barclay_adaptor(file_path : str):
+    '''
+    BarclayCard
+    export as CSV file
+    header with Barclays Bank Delaware, Account number (last four digits), Account Balance as of DATE: AMOUNT
+    Transaction Date, Description, Amount, Category (DEBIT OR CREDIT)
+    '''
+    transaction_list = []
+    with open(file_path) as barclay_csv:
+        barclay_data = list(csv.DictReader(barclay_csv, fieldnames = ["Transaction Date", "Description",
+        "Category", "Amount"]))
+    
+    for row in barclay_data:
+        for k, v in row.items():
+            if None in [k, v] or k == v:
+                break
+        else:
+            transaction_list.append(make_transaction_from_barclay(row))
+    return transaction_list
 
-# Category (not included in CSV)
-# Automotive
-    # oil changes
-    # car washes
-    # tires
-# Healthcare
-# doctor visits
-# prescriptions
-# Merchandise
-    # mall
-    # online
-    # other retail venues
-# Restaurants & Entertainment
-    # meals
-    # movies
-    # sporting events
-# Travel
-# airlines
-    # car rentals
-    # lodging
-    # railroad tickets
-    # cruises
-# Everyday Spending
-    # gas
-    # groceries
-    # utilities
-# Other
-    # home improvement
-    # education
+def make_transaction_from_barclay(barclay_transaction):
+    date = change_date(barclay_transaction["Transaction Date"])
+    amount = -change_amount(barclay_transaction["Amount"])
+    merchant = change_merchant(barclay_transaction["Description"])
+    transaction_object = Transaction(date = date, amount = amount, merchant = merchant)
+    return transaction_object
+
+
+def chase_adaptor(file_path : str):
+    '''
+    Chase
+    export as CSV file
+    Type, Trans Date, Post Date, Description, Amount, Category, Memo
+    '''
+    transaction_list = []
+    with open(file_path) as chase_csv:
+        chase_data = list(csv.DictReader(chase_csv))
+    
+    for row in chase_data:
+        transaction_list.append(make_transaction_from_chase(row))
+    return transaction_list
+
+def make_transaction_from_chase(chase_transaction):
+    date = change_date(chase_transaction["Trans Date"])
+    amount = -change_amount(chase_transaction["Amount"])
+    merchant = change_merchant(chase_transaction["Description"])
+    transaction_object = Transaction(date = date, amount = amount, merchant = merchant)
+    return transaction_object
