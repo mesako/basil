640d4953ead2943453d9277300c162d10cfd0e66
Melissa Ko <mesako@stanford.edu>
2017-04-29 19:01:41 -0700

    convert matrices to pandas df

--- convert_transactions_to_matrix.py
+++ convert_transactions_to_matrix.py
@@ -1,4 +1,5 @@
 import numpy
+import pandas as pd
 
 def convert_transaction_list_to_matrix(transaction_list):
     date_vec = []
@@ -7,7 +8,6 @@ def convert_transaction_list_to_matrix(transaction_list):
     desc_vec = []
     category_vec = []
     for t in transaction_list:
-        print(t)
         date_vec.append(t.date)
         merchant_vec.append(t.merchant)
         amount_vec.append(t.amount)
@@ -17,3 +17,13 @@ def convert_transaction_list_to_matrix(transaction_list):
     transaction_matrix = numpy.matrix([date_vec, merchant_vec, amount_vec, desc_vec, category_vec])
     transaction_matrix = transaction_matrix.T
     return(transaction_matrix)
+
+def convert_transaction_matrix_to_df(transaction_matrix):
+    transaction_df = pd.DataFrame(data = transaction_matrix[:,:], columns = ["Date", "Merchant", "Amount", "Description", "Category"])
+    return(transaction_df)
+
+def convert_transaction_list_to_df(transaction_list):
+    transaction_matrix = convert_transaction_list_to_matrix(transaction_list)
+    transaction_df = convert_transaction_matrix_to_df(transaction_matrix)
+    return(transaction_df)
+
